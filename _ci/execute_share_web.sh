a59e26dcc6278e5dead42ce8cbd8a0995d3b77f5
araschitor <andrei.raschitor@ness.com>
2021-02-19 12:58:12 +0200

    first run

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-tas-share-test.git
 cd alfresco-tas-share-test
-git checkout fix/APPS-get-rid-of-duplication
+git checkout fix/APPS-refactored-model-action-component
 
 if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
   NAMESPACE="develop-share"
