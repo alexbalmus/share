d4e240029aa37738854d0bb3a0774f37de3e84ad
araschitor <andrei.raschitor@ness.com>
2021-02-23 22:07:49 +0200

    fix/APPS-test-ready-dom-state: added wait until dom ready state == complete

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-tas-share-test.git
 cd alfresco-tas-share-test
-git checkout master
+git checkout fix/APPS-dom-complete-state
 
 if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
   NAMESPACE="develop-share"
