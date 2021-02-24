787e0622458375fc3b40f49678aea17556309968
araschitor <andrei.raschitor@ness.com>
2021-02-24 20:19:39 +0200

    fix/APPS-retry-listener: checkout retry listener branch

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-tas-share-test.git
 cd alfresco-tas-share-test
-git checkout master
+git checkout fix/APPS-add-retry
 
 if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
   NAMESPACE="develop-share"
