8b4579708fdc72326f0bef5b9b51f10d97ec39de
araschitor <71637144+araschitor@users.noreply.github.com>
2021-03-17 19:13:48 +0200

    fix/APPS-allure: allure serve report

--- _ci/test-reports.sh
+++ _ci/test-reports.sh
@@ -1,2 +1,3 @@
-allure generate -c alfresco-tas-share-test/target/surefire-reports -o reports/allure-results-html
-aws s3 sync reports/allure-results-html s3://test-report-allure/reports/
+#allure generate -c alfresco-tas-share-test/target/surefire-reports -o reports/allure-results-html
+#aws s3 sync reports/allure-results-html s3://test-report-allure/reports/
+allure serve /alfresco-tas-share-test/target/surefire-reports
