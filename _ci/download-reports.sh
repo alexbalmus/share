812ae6a0dccef4e2429e15e6ea7da9972b71a436
Piero Cittadino <63122825+pieCit87@users.noreply.github.com>
2021-03-18 00:30:49 +0200

    fixes

--- _ci/download-reports.sh
+++ _ci/download-reports.sh
@@ -1,5 +1,10 @@
+source _ci/init_tag.sh
+
 aws s3 sync s3://test-report-allure/$TRAVIS_BUILD_NUMBER reports/Share/$TAG_NAME
 cd reports/Share/$TAG_NAME
 export LIST=$(ls)
+
+echo $LIST
+
 allure generate -c $LIST -o allure/Share/$TAG_NAME
 aws s3 sync  allure/Share/$TAG_NAME s3://test-report-allure/final-report/Share/$TAG_NAME
