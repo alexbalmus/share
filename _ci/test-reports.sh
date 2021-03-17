30140011a8309a8a0d7b00cd31ee4db8a7f0b6c1
araschitor <71637144+araschitor@users.noreply.github.com>
2021-03-17 08:53:44 +0200

    fix/APPS-allure: added open allure report

--- _ci/test-reports.sh
+++ _ci/test-reports.sh
@@ -1,3 +1,3 @@
-allure serve alfresco-tas-share-test/target/surefire-reports
 allure generate -c reports/allure-results -o reports/allure-results-html
 aws s3 sync reports/allure-results-html s3://test-report-allure/reports/
+allure open reports
