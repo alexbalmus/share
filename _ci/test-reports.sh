7d498140c8cb7c9cfddf238cd4c00583f6dc9424
Piero Cittadino <63122825+pieCit87@users.noreply.github.com>
2021-03-17 22:29:19 +0200

    quick adaptions

--- _ci/test-reports.sh
+++ _ci/test-reports.sh
@@ -1,3 +1,4 @@
-#allure generate -c alfresco-tas-share-test/target/surefire-reports -o reports/allure-results-html
-#aws s3 sync reports/allure-results-html s3://test-report-allure/reports/
-allure serve /alfresco-tas-share-test/target/surefire-reports
+source _ci/init_tag.sh
+
+allure generate -c alfresco-tas-share-test/target/surefire-reports -o reports/Share/$TAG_NAME
+aws s3 sync reports/Share/$TAG_NAME s3://test-report-allure/reports/Share/$TAG_NAME
