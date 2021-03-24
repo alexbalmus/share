f8e9536b98ffccbda185fcdf0216aaba42112dc2
araschitor <71637144+araschitor@users.noreply.github.com>
2021-03-24 11:07:19 +0200

    feature/APPS-907-allure-readme: test if screenshots of failed tests are uploaded into /report

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-tas-share-test.git
 cd alfresco-tas-share-test
-git checkout master
+git checkout feature/APPS-delete-html-reporter
 
 if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
   NAMESPACE="develop-share"
